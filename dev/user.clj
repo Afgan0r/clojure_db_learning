7da6d09dee5702b265086da6c203a996270b5a0a
Alexandr Pavlov <pavlov_aleksandr_2001@mail.ru>
2019-12-30 14:06:45 +0700

    add new table, reference and query with inner join

--- dev/user.clj
+++ dev/user.clj
@@ -2,11 +2,20 @@
   (:require [learning-db.core :as core]))
 
 (defn create-and-fill-database []
-  (core/create-students-table)
-  (core/add-students))
+  (core/create-tables)
+  (core/add-groups-and-students))
 
 (defn print-last-and-first-name [{:keys [first_name last_name]}]
-  (println (str "First name is: " first_name ". Last name is: " last_name)))
+  (println (str "Student name is: " first_name " " last_name ".")))
+
+(defn print-student-name-and-his-group [{:keys [first_name last_name group_name]}]
+  (println (str "Student name is: " first_name " " last_name ", his group is: " group_name)))
 
 (defn get-students []
-  (doseq [curr-student (core/get-all-students)] (print-last-and-first-name curr-student)))
+  (println "Students names:")
+  (doseq [curr-student (core/get-all-students-with-groups)] 
+    (print-last-and-first-name curr-student))
+  (println "Students names and his groups:")
+  (doseq [curr-student (core/get-all-students-with-groups)]
+    (print-student-name-and-his-group curr-student))
+  )
--- src/learning_db/core.clj
+++ src/learning_db/core.clj
@@ -9,35 +9,50 @@
 
 (def ds (jdbc/get-datasource db))
 
-(defn create-students-table []
+(defn create-tables []
   (jdbc/execute! ds ["
 DROP TABLE IF EXISTS Students;
+DROP TABLE IF EXISTS Groups;
+
+CREATE TABLE Groups (
+id_group INT AUTO_INCREMENT PRIMARY KEY,
+curator_name varchar(32),            
+group_name varchar (10)
+);
+
 CREATE TABLE Students(
 id_student INT AUTO_INCREMENT PRIMARY KEY,
 first_name varchar(32),
 last_name varchar(32),
 age int,
-email varchar(255)
-);"]))
+email varchar(255),
+group_id int,
+FOREIGN KEY (group_id) REFERENCES Groups (id_group)
+);
+"]))
 
-(defn add-students []
-  (let [insert (-> (insert-into :Students)
-                   (columns :first_name :last_name :age :email)
-                   (values 
-                    [["Alexandr", "Pavlov", "18", "somemail @mail.ru"]
-                     ["Artem", "Guderian","17","ag_mail @google.com"]])
-                   sql/format)]
-    (jdbc/execute! ds insert)))
+(defn add-groups-and-students []
+  (let [insert-groups (-> (insert-into :Groups)
+                          (columns :curator_name :group_name)
+                          (values 
+                           [["Evdokim Leontevich", "9ISiP171"]
+                            ["Filimon Andreevich", "11PO191"]])
+                          sql/format)
+        insert-students (-> (insert-into :Students)
+                            (columns :first_name :last_name :age :email :group_id)
+                            (values
+                             [["Alexandr", "Pavlov", "18", "somemail @mail.ru", 1]
+                              ["Artem", "Guderian","17","ag_mail @google.com", 1]])
+                            sql/format)]
+    (jdbc/execute! ds insert-groups)
+    (jdbc/execute! ds insert-students)))
 
-(defn get-all-students []
-  (let [query (-> (select :first_name :last_name)
+(defn get-all-students-with-groups []
+  (let [query (-> (select :*)
                   (from :Students)
+                  (join :Groups [:= :group_id :id_group])
                   sql/format)]
     (jdbc/execute! ds query
                    {:builder-fn rs/as-unqualified-lower-maps})))
 
-(defn print-last-and-first-name [{:keys [first_name last_name]}]
-  (println (str "First name is: " first_name ". Last name is: " last_name)))
-
-(defn -main [& args]
-  (doseq [curr-student (get-all-students)] (print-last-and-first-name curr-student)))
+(defn -main [& args])
